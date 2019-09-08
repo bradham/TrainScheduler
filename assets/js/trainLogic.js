06e508c307b1bd5d9727c48b99b2183c4ad21ece
bradham <bradham@users.noreply.github.com>
2019-09-07 23:25:05 -0500

    Added train input card with submit button connecting to database. Need to refactor variable and id names

--- assets/js/trainLogic.js
+++ assets/js/trainLogic.js
@@ -20,18 +20,30 @@ firebase.initializeApp(firebaseConfig);
 
 var dataBase = firebase.database();
 
-    // Initial Values
-    var name = "test name";
-    var email = "test@test.com";
-    var age = 0;
-    var comment = "test comment";
-      
+// Initial Values
+var name = "test2 name";
+var email = "test2@test.com";
+var age = 2;
+var comment = "test2 comment";
+
+
+// Capture Button Click
+$("#add-train").on("click", function (event) {
+    event.preventDefault();
+
+    //Get new train data from input values
+    name = $("#name-input").val().trim();
+    email = $("#email-input").val().trim();
+    age = $("#age-input").val().trim();
+    comment = $("#comment-input").val().trim();
     // Code test for the push
-      dataBase.ref().push({
+    dataBase.ref().push({
 
         name: name,
         email: email,
         age: age,
         comment: comment,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+    });
+});
+
