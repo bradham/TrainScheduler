b18b11a8a6a62c5e0fc02d87e1b93b162f1a141c
bradham <bradham@users.noreply.github.com>
2019-09-10 23:54:05 -0500

    added calculations for arrival time and minutes away

--- assets/js/trainLogic.js
+++ assets/js/trainLogic.js
@@ -58,16 +58,18 @@ $("#add-train").on("click", function (event) {
         console.log("Frequency: " + childSnapshot.val().freq);
         console.log("Date added: " + childSnapshot.val().dateAdded);
 
-        //calculate next arrival time and minutes away
-        var nextArrival = calcArrival(300, 5);
-        var minAway = calcMinutes(nextArrival);
+        //calculate next arrival time and minutes away.
+        //calcArrival feurns an array with both arrival time and minutes away
+        var nextArrival = calcArrival(childSnapshot.val().firstTime, childSnapshot.val().freq);
+        var minAway = nextArrival[1];
+        //calcMinutes(nextArrival);
     
   
         // Add a row to the table
         var data = "<td>" + childSnapshot.val().name + "</td>";
             data+= "<td>" + childSnapshot.val().dest + "</td>";
             data+= "<td>" + childSnapshot.val().freq + "</td>";
-            data+= "<td>" + nextArrival + "</td>";
+            data+= "<td>" + nextArrival[0] + "</td>"; //from calcArrival array
             data+= "<td>" + minAway + "</td>";
 
         //DEBUG LOG
@@ -85,7 +87,8 @@ $("#add-train").on("click", function (event) {
         console.log("Errors handled: " + errorObject.code);
       });
   
-/*       dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+/*      For showing most recent db record 
+        dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
         // Change the HTML to reflect
         $("#name-display").text(snapshot.val().name);
         $("#email-display").text(snapshot.val().email);
@@ -96,11 +99,52 @@ $("#add-train").on("click", function (event) {
 
  function calcArrival(time, freq) {
      //calculate arrival time. return formatted time
-     return "2:00 PM";
+     console.log(moment().format('Hm'));
+     var now = moment().format('Hm');
+     var nextTime = parseInt(now) + freq;
+     console.log(nextTime);
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    //var timeConverted = moment(time, "hh:mm").subtract(1, "years");
+    //console.log("timeConverted: " + timeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HHmm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(time), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % freq;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = freq - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+     return [moment(nextTrain).format("hh:mm"), tMinutesTillTrain];
  };
 
  function calcMinutes(time) {
      //calculate minutes away from current time and return number of minutes
-     return 3;
+         // Difference between the times
+    // var diffTime = moment().diff(moment(time), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // // Time apart (remainder)
+    // var tRemainder = diffTime % freq;
+    // console.log(tRemainder);
+
+    // // Minute Until Train
+    // var tMinutesTillTrain = freq - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+     return 5;
  };
 
